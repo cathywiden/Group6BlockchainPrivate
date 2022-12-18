065c5dbc5463bcf819c70dec1ce75c27cf659749
FredrikCarlssn <113025364+FredrikCarlssn@users.noreply.github.com>
2022-12-18 16:05:12 +0100

    Created the CalculateHash function

--- src/script.js
+++ src/script.js
@@ -3,22 +3,21 @@
 //=====================================//
 
 import { rollApi } from "/src/api/locationScript.js";
+import { calculateHash } from "/src/blockchain/calculateHash.js";
 //import { helloBlock } from "/src/blockchain/block.mjs";
-
 
 //=====================================//
 //===========GLOBAL VARIABLES==========//
 //=====================================//
 
-let searchButton = document.getElementsByClassName('searchButton')[0];
-let input = document.getElementsByClassName('searchInput')[0];
-
+let searchButton = document.getElementsByClassName("searchButton")[0];
+let input = document.getElementsByClassName("searchInput")[0];
 
 //=====================================//
 //=============RUNNING SCRIPT==========//
 //=====================================//
 
-
 rollApi(searchButton, input);
 
-console.log("The rollAPI-function is connected " +rollApi);
+console.log("The rollAPI-function is connected " + rollApi);
+console.log("Encrypt: Hello World! ===", await calculateHash("Hello World!"));
