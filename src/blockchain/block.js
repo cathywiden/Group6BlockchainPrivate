104b85491da99cb28105d48a19d50490fceac87b
Jakob Dahlberg <dahlbergjakob@hotmail.com>
2022-12-18 20:10:10 +0100

    block.js update 1
    Block Method, Start of newDate

---
+++ src/blockchain/block.js
@@ -0,0 +1,16 @@
+class Block {
+  constructor(data, previousHash) {
+    this.data = data;
+    this.previousHash = previousHash;
+    this.timestamp = ""; // newDate() funktion
+    this.hash = ""; // calculatehash() funktion
+  }
+
+  newDate() {
+    // METOD
+  }
+
+  calculateHash() {
+    //METOD
+  }
+}
