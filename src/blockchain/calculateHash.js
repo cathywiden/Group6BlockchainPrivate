e831ed199b8ea0ad378acb4d37782ff4d2bce265
FredrikCarlssn <113025364+FredrikCarlssn@users.noreply.github.com>
2022-12-26 16:40:35 +0100

    Added salt

--- src/blockchain/calculateHash.js
+++ src/blockchain/calculateHash.js
@@ -1,9 +1,7 @@
 export async function calculateHash(data) {
-
-
   //Basically Janne's kod except that I converted all data to JSON.
-  
-  const dataString = JSON.stringify(data); // to me (Cathy), the concatenated hash function always resulted in the exact same hash from block 2 onwards, that's why I stringified it instead. 
+
+  const dataString = JSON.stringify(data + Math.random()); // to me (Cathy), the concatenated hash function always resulted in the exact same hash from block 2 onwards, that's why I stringified it instead. Math.random() adds salt.
 
   // Encode string as an array of 8-bit integers
   const messageInt8 = new TextEncoder().encode(dataString);
@@ -15,12 +13,13 @@ export async function calculateHash(data) {
   const hashArray = Array.from(new Uint8Array(hashBuffer));
 
   // Convert each integer in the array to a hexadecimal string
-  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
 
   // Return the hash as a hex string
   return hashHex;
 }
-
 
 /* export async function calculateHash(data, previousHash) {
   let message = data + previousHash;
@@ -30,4 +29,3 @@ export async function calculateHash(data) {
   let hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
   return hashHex;
 } */
-
